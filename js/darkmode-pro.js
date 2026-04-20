6ae16ae6ec795148a3a1e68fdfa8ee16ccdf8b73
marisalozan-dev <marisalozan@gmail.com>
2026-04-20 19:06:42 +0200

    Update darkmode-pro.js

--- js/darkmode-pro.js
+++ js/darkmode-pro.js
@@ -10,3 +10,16 @@ toggle.onclick = () => {
     document.body.classList.contains("dark") ? "dark" : "light"
   );
 };
+// --- DONUT MODO OSCURO ---
+function updateDonutColors(isDark) {
+  if (!ctx) return;
+
+  const newColors = isDark
+    ? ['#27ae60', '#d4ac0d']   // versión oscura
+    : ['#2ecc71', '#f1c40f'];  // versión clara
+
+  donutChart.data.datasets[0].backgroundColor = newColors;
+  donutChart.update();
+}
+updateDonutColors(document.body.classList.contains('dark'));
+
