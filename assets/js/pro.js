f8873059ba3ff0d1e8b4b5c961673c3e9fdc9138
marisalozan-dev <marisalozan@gmail.com>
2026-04-21 23:22:40 +0200

    Update pro.js

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -1,4 +1,12 @@
-console.log("DocuStream PRO cargado correctamente");
+console.log("DocuStream cargado correctamente");
+
+// Dark mode
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
 
 
 
