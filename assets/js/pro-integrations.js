1838a4c1825a485d3b44058e15168bb29ce499aa
marisalozan-dev <marisalozan@gmail.com>
2026-04-22 22:08:34 +0200

    Create pro-integrations.js

---
+++ assets/js/pro-integrations.js
@@ -0,0 +1,7 @@
+console.log("Integraciones PRO loaded");
+
+function initIntegrations() {
+    console.log("Inicializando Integraciones PRO…");
+    const grid = document.getElementById("integrationsGrid");
+    if (grid) grid.innerHTML = "<p>Integraciones cargadas (demo).</p>";
+}
