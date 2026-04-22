f362081192d76552dd9471b74a97fc1a701c0a66
marisalozan-dev <marisalozan@gmail.com>
2026-04-22 22:07:44 +0200

    Create pro-dashboard.js

---
+++ assets/js/pro-dashboard.js
@@ -0,0 +1,9 @@
+console.log("Dashboard PRO loaded");
+
+function initDashboard() {
+    console.log("Inicializando dashboard…");
+}
+
+function updateDashboard(state) {
+    console.log("Actualizando dashboard con filtros:", state);
+}
